a272afefa254db776a10af5fdfeb40fd2484ad66
Łukasz Indykiewicz <lukasz.indykiewicz@blstream.com>
2016-01-08 16:43:37 +0100

    Add the most basic search

--- build.sbt
+++ build.sbt
@@ -9,3 +9,6 @@ sbtPlugin := true
 // Scripted - sbt plugin tests
 scriptedSettings
 scriptedLaunchOpts <+= version apply { v => "-Dproject.version="+v }
+
+libraryDependencies += "net.liftweb" %% "lift-json" % "2.6.2"
+
--- src/main/scala/com/blstream/sbtsearchmavenplugin/SbtSearchMavenPlugin.scala
+++ src/main/scala/com/blstream/sbtsearchmavenplugin/SbtSearchMavenPlugin.scala
@@ -2,9 +2,10 @@ package com.blstream.sbtsearchmavenplugin
 
 import sbt._
 import sbt.complete._
-import complete.DefaultParsers._
+//import complete.DefaultParsers._
 import sbt.Keys.{ streams, target }
 import java.nio.charset.Charset
+import net.liftweb.json._
 
 object SbtSearchMavenPlugin extends AutoPlugin {
 
@@ -15,11 +16,24 @@ object SbtSearchMavenPlugin extends AutoPlugin {
   import autoImport._
 
   override lazy val projectSettings = Seq(
-    searchMaven := search(spaceDelimited("<arg>").parsed, streams.value.log)
+    searchMaven := search(complete.DefaultParsers.spaceDelimited("<arg>").parsed, streams.value.log)
   )
 
   def search(args: Seq[String], log: Logger) {
-    args foreach (arg => log info arg)
+
+    implicit val formats = DefaultFormats
+
+    val found = args.headOption.map { head =>
+      val query = s"http://search.maven.org/solrsearch/select?q=$head&rows=20&wt=json"
+      val results = scala.io.Source.fromURL(query).mkString
+      val json = parse(results)
+      val artifacts = json \ "response" \ "docs"
+      artifacts.extract[List[Artifact]]
+    }
+
+    found.map(_.map(a => s"${a.g} %% ${a.a} % ${a.latestVersion}").foreach(x => log info x))
   }
 
 }
+
+case class Artifact(g: String, a: String, latestVersion: String)
